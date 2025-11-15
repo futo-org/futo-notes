af4d046481c45be2918b452aa94ac37384b3ce3d
Justin Fowler <justin@futo.org>
2025-11-15 12:58:17 -0600

    basic note view

--- app/src/main/java/org/futo/notes/MainActivity.kt
+++ app/src/main/java/org/futo/notes/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -23,8 +24,10 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.selects.select
 import org.futo.notes.core.Note
 import org.futo.notes.ui.theme.FUTONotesTheme
 
@@ -44,22 +47,36 @@ class MainActivity : ComponentActivity() {
 fun NotesApp() {
     var text by remember { mutableStateOf("") }
     val notes = remember { mutableStateListOf<Note>() }
+    var selectedNote by remember {mutableStateOf<Note?>(null)}
 
+    if (selectedNote === null) {
+        NotesList(text, notes, onTextChange = {text = it}, onAddNote = {
+            if (text.isNotBlank()) {
+                notes.add(Note(title=text))
+                text = ""
+            }
+        }, onNoteClick = {note ->
+            selectedNote = note})
+    } else {
+        NotesDetailScreen(selectedNote!!, onBack = {selectedNote= null})
+    }
+
+
+
+
+}
+
+@Composable
+fun NotesList(text: String, notes: List<Note>, onTextChange: (String) -> Unit, onAddNote: () -> Unit, onNoteClick: (Note) -> Unit) {
     Column(Modifier.padding(16.dp)) {
         Row {
             TextField(
                 value = text,
-                onValueChange = { text = it},
+                onValueChange = onTextChange,
                 modifier = Modifier.weight(1f)
             )
             Button(
-                onClick = {
-                    if (text.isNotBlank()) {
-                        notes.add(Note(title = text))
-                        text = ""
-                    }
-                },
-                modifier = Modifier.padding(start = 8.dp)
+                onClick = onAddNote , modifier = Modifier.padding(start = 8.dp)
             ) {
                 Text ("Add")
             }
@@ -68,8 +85,24 @@ fun NotesApp() {
 
         LazyColumn {
             items(notes) { note ->
-                Text(note.title, Modifier.padding(vertical = 4.dp))
+                Text(note.title, Modifier
+                    .padding(vertical = 4.dp)
+                    .clickable { onNoteClick(note) })
             }
         }
+    }
+
+}
+
+@Composable
+fun NotesDetailScreen(note: Note, onBack: () -> Unit) {
+    Column(modifier = Modifier.fillMaxSize()) {
+        Button(onClick = onBack) {
+            Text("Back")
+        }
+        Spacer(Modifier.height(16.dp))
+
+        Text(text = note.title)
+        Text(text = note.body)
     }
 }
--- app/src/main/java/org/futo/notes/core/Note.kt
+++ app/src/main/java/org/futo/notes/core/Note.kt
@@ -5,7 +5,7 @@ import java.util.UUID
 data class Note (
     val id: String = UUID.randomUUID().toString(),
     val title: String,
-    val body: String = "",
+    val body: String = "my body",
     val createdAt: Long = System.currentTimeMillis(),
     val updatedAt: Long = System.currentTimeMillis(),
     val isDeleted: Boolean = false
